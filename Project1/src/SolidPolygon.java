b7afd7b500aa0ab7f29184e10ebeaad4014e7427
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2024-10-12 14:19:41 +0000

    Added two of the required classes
    feat: Added Text and solid polygon classes

---
+++ Project1/src/SolidPolygon.java
@@ -0,0 +1,13 @@
+
+import java.awt.*;
+
+public class SolidPolygon extends Polygon_ {
+
+    public SolidPolygon(Color color, int vertexCount) {
+        super(color, vertexCount);
+    }
+
+    public void drawPolygon(Graphics graphics, Polygon polygon) {
+        // Graphics.fillPolygon() 
+    }
+}
---
+++ Project1/src/Text.java
@@ -0,0 +1,16 @@
+
+import java.awt.*;
+
+public class Text extends Image {
+
+    public Text(Color color, Point point, String data) {
+        super(color);
+
+    }
+
+    @Override
+    void draw(Graphics graphics) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+}
